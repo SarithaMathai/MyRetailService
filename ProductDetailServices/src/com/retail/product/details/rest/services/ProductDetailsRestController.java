50a055975534a134c03c7d113fd95dd467ac3e88
SarithaMathai <smathaig@gmail.com>
2016-10-11 21:46:01 -0500

    Update ProductDetailsRestController.java

--- ProductDetailServices/src/com/retail/product/details/rest/services/ProductDetailsRestController.java
+++ ProductDetailServices/src/com/retail/product/details/rest/services/ProductDetailsRestController.java
@@ -1,5 +1,7 @@
 package com.retail.product.details.rest.services;
 
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,19 +17,18 @@ import com.retail.product.details.processor.ProductDetailsProcessor;
 @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 
 public class ProductDetailsRestController {
-    @GET
-    @Path("/{id}")
-    public ProductDetailsItem getProductDtl(@PathParam("id") int id) {
-        try {
-            ProductDetailsItem prodDtls = new ProductDetailsProcessor().getProductDetails(id);
-            return prodDtls;
+	final static Logger logger = Logger.getLogger(ProductDetailsRestController.class.getName());
 
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return null;
+	@GET
+	@Path("/{id}")
+	public ProductDetailsItem getProductDtl(@PathParam("id") int id) {
+		try {
+			ProductDetailsItem prodDtls = new ProductDetailsProcessor().getProductDetails(id);
+			return prodDtls;
 
-    }
-
+		} catch (Exception e) {
+			logger.error("Exception occurred in ProductDetailsItem for REST call", e);
+		}
+		return null;
+	}
 }
